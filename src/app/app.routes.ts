e711907b4a4bcaeaac8bc69228ccb7d9cbaed2a7
Johannes Roth <jrothx@icloud.com>
2025-03-06 17:35:20 +0100

    legal components added

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,10 +3,14 @@ import { MainComponent } from './main/main.component';
 import { JoinComponent } from './join/join.component';
 import { PollolocoComponent } from './polloloco/polloloco.component';
 import { DabubbleComponent } from './dabubble/dabubble.component';
+import { ImprintComponent } from './legal/imprint/imprint.component';
+import { PrivacypolicyComponent } from './legal/privacypolicy/privacypolicy.component';
 
 export const routes: Routes = [
     { path: '', component: MainComponent },
     { path: 'join', component: JoinComponent },
     { path: 'polloloco', component: PollolocoComponent },
     { path: 'dabubble', component: DabubbleComponent },
+    { path: 'imprint', component: ImprintComponent },
+    { path: 'privacy', component: PrivacypolicyComponent }
 ];
---
+++ src/app/legal/privacypolicy/privacypolicy.component.ts
@@ -0,0 +1,12 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-privacypolicy',
+  imports: [CommonModule],
+  templateUrl: './privacypolicy.component.html',
+  styleUrl: './privacypolicy.component.scss'
+})
+export class PrivacypolicyComponent {
+
+}
