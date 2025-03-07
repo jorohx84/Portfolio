da839b711aa7f3852a22f5da0f7d75ce67bfdad1
Johannes Roth <jrothx@icloud.com>
2025-03-07 21:11:23 +0100

    save

--- src/app/sendMail.php
+++ src/app/sendMail.php
@@ -18,7 +18,7 @@ switch ($_SERVER['REQUEST_METHOD']) {
             $name = $params->name;
             $message = $params->message;
     
-            $recipient = 'jrothx@icloud.com';  
+            $recipient = 'kontakt@johannes-roth.de';  
             $subject = "Contact From <$email>";
             $message = "From:" . $name . "<br>" . $message ;
     
